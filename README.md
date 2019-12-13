a8857c568c047f1df2f4a383250cf60fdcfeddea
haykzaryan <hayk.zaryan@gmail.com>
2019-12-13 01:37:53 +0100

    First version!

---
+++ README.md
@@ -0,0 +1,26 @@
+![alt text](https://d2slcw3kip6qmk.cloudfront.net/marketing/blog/2017Q2/project-planning-header@2x.png)
+
+| [Docker](https://www.docker.com/) |
+
+### Description
+  
+
+### Installation
+
+**Reload the user's group assignments if you are not in "docker" group**
+
+	su - $(whoami)
+
+**Clone the repository -> Enter to the folder -> Change permissions of scripts -> Execute the script1**
+
+	git clone https://github.com/haykzaryan/projecte.ftpd.git && cd projecte.ftpd && chmod 777 projecte.ftpd.sh && ./projecte.ftpd.sh
+
+
+**That's it! Thank you.** 
+
+![alt text](https://res.cloudinary.com/teepublic/image/private/s--vzzS_Z2T--/t_Preview/b_rgb:191919,c_limit,f_jpg,h_630,q_90,w_630/v1567163291/production/designs/5791500_0.jpg)
+
+  
+ 
+ 
+ 
---
+++ projecte.ftpd.sh
@@ -0,0 +1,78 @@
+#!/bin/bash
+
+echo
+sleep 1
+echo " *"
+sleep 1
+echo " *"
+sleep 1
+echo " *** Welcome to Projecte.ftpd script made by haykzaryan ***"
+sleep 2
+echo " *** Have a nice day! ***"
+echo
+echo
+
+#Creates volume (deletes the old volume and containers in case if you have already executed the script 1 or more times)
+docker stop apache.ftp server.ftp client.ftp web1.ftp web2.ftp web3.ftp
+docker rm apache.ftp server.ftp client.ftp web1.ftp web2.ftp web3.ftp
+docker volume rm volume.ftp
+docker volume create volume.ftp
+
+#Creates ftp server
+docker run -d -p 21:21 -v volume.ftp:/home/vsftpd/myuser --name server.ftp -e FTP_USER=cliente -e FTP_PASS=clientji* fauria/vsftpd
+ 
+#Creates ftp client
+docker run -d --name client.ftp -p 5800:5800 -v /docker/appdata/filezilla:/config:rw -v $HOME:/storage:rw jlesage/filezilla
+
+#Creates Apache container with our volume, mounted in /usr/local/apache2/htdocs/
+docker run -dit --name  apache.ftp  -v volume.ftp:/usr/local/apache2/htdocs/ httpd:2.4
+
+#Creates apache for websites
+docker run -dit --name  web1.ftp httpd:2.4
+docker run -dit --name  web2.ftp httpd:2.4
+docker run -dit --name  web3.ftp httpd:2.4
+
+# INFO
+echo
+echo
+echo
+echo
+echo
+echo
+echo " *****   IMPORTANT!   *****"
+echo
+sleep 1
+echo " --> Files of your host's directory [/home/$(whoami)], it shows in "
+echo " /storage (Host directory has been mounted in Client FTP's /storage directory.) "  
+echo
+sleep 1
+echo "Your Client FTP's IP Address:" $(docker exec client.ftp sh && for i in $(echo $(ip a | grep 172)); do echo $i; done | grep /16) 
+echo
+sleep 1
+echo "Your Server FTP's IP Address:" $(docker exec server.ftp hostname -I)
+sleep 1
+echo "Your Client FTP's User: cliente"
+sleep 1
+echo "Your Client FTP's User's password: clientji*"
+sleep 1
+echo
+sleep 1
+echo "Your Apache's IP Address:" $(docker exec apache.ftp hostname -I)
+echo
+echo
+echo " ***** You can already access to your Server FTP from Client FTP putting User's "
+echo "       login and password. ***** "
+echo
+echo
+echo
+cd .. 
+rm -rf projecte.ftpd
+ 
+
+ 
+
+
+
+
+
+
