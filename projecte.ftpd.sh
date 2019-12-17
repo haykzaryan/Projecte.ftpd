48235e1bd6e20fcdffebfeebbbc621abed822359
haykzaryan <hayk.zaryan@gmail.com>
2019-12-17 01:25:08 +0100

    Updated!

--- projecte.ftpd.sh
+++ projecte.ftpd.sh
@@ -13,13 +13,13 @@ echo
 echo
 
 #Creates volume (deletes the old volume and containers in case if you have already executed the script 1 or more times)
-docker stop apache.ftp server.ftp client.ftp web1.ftp web2.ftp web3.ftp
-docker rm apache.ftp server.ftp client.ftp web1.ftp web2.ftp web3.ftp
+docker stop apache.ftp server.ftp client.ftp web1.ftp web2web3.ftp
+docker rm apache.ftp server.ftp client.ftp web1.ftp web2web3.ftp
 docker volume rm volume.ftp
 docker volume create volume.ftp
 
 #Creates ftp server
-docker run -d -p 21:21 -v volume.ftp:/home/vsftpd/myuser --name server.ftp -e FTP_USER=cliente -e FTP_PASS=clientji* fauria/vsftpd
+docker run -d -p 21:21 -v volume.ftp:/home/vsftpd/admin --name server.ftp -e FTP_PASS=clientji* fauria/vsftpd
  
 #Creates ftp client
 docker run -d --name client.ftp -p 5800:5800 -v /docker/appdata/filezilla:/config:rw -v $HOME:/storage:rw jlesage/filezilla
@@ -28,8 +28,20 @@ docker run -d --name client.ftp -p 5800:5800 -v /docker/appdata/filezilla:/confi
 docker run -dit --name  apache.ftp  -v volume.ftp:/usr/local/apache2/htdocs/ httpd:2.4
 
 #Creates apache for websites
-docker run -dit --name  web1web2.ftp httpd:2.4
-docker run -dit --name  web3.ftp httpd:2.4
+docker run -dit --name  web1.ftp httpd:2.4
+docker run -dit --name  web2web3.ftp httpd:2.4
+
+#Create subfolders for each website (web2web3.ftp)
+docker exec web2web3.ftp bash -c "cd /usr/local/apache2/htdocs && mkdir web2 web3"
+
+#Copy websites to our servers (web1.ftp and web2web3.ftp)
+echo First website directory: ; read web1_dir
+echo Second website directory: ; read web2_dir
+echo Third website directory: ; read web3_dir
+cd $web1_dir && docker cp . web1.ftp:/usr/local/apache2/htdocs/
+cd $web2_dir && docker cp . web2web3.ftp:/usr/local/apache2/htdocs/web2
+cd $web3_dir && docker cp . web2web3.ftp:/usr/local/apache2/htdocs/web3
+docker exec web2web3.ftp bash -c "cd /usr/local/apache2/htdocs && rm index.html"
 
 # INFO
 echo
@@ -50,13 +62,15 @@ echo
 sleep 1
 echo "Your Server FTP's IP Address:" $(docker exec server.ftp hostname -I)
 sleep 1
-echo "Your Client FTP's User: cliente"
+echo "Your Client FTP's User: admin"
 sleep 1
 echo "Your Client FTP's User's password: clientji*"
 sleep 1
 echo
 sleep 1
 echo "Your Apache's IP Address:" $(docker exec apache.ftp hostname -I)
+sleep 1
+echo "Your Client Apache's IP Adresses:" $(docker exec web1.ftp hostname -I) $(docker exec web2web3.ftp hostname -I)
 echo
 echo
 echo " ***** You can already access to your Server FTP from Client FTP putting User's "
